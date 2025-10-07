41dd8cc5ad7c665e7f98f344b0b131d3008a9cf0
NhanNguyen-code <nguyennhanpro47@gmail.com>
2025-10-07 18:05:41 +0700

    Update ProjectLeader.java

---
+++ BT3/ProjectLeader.java
@@ -0,0 +1,11 @@
+class ProjectLeader extends Programmer{
+
+	public ProjectLeader(int thePayrollNumber, String theName, double theBasicMohthlySalary, String theLanguage) {
+		super(thePayrollNumber, theName, theBasicMohthlySalary, theLanguage);
+	}
+	
+	@Override
+	double getMonthlySalary() {
+		return super.getMonthlySalary() * 1.2;
+	}
+}
